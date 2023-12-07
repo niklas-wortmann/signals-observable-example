71196d9c834e0b75fda870fcb1df0bbf1c29f26e
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2023-12-06 22:52:38 -0600

    implement signalStore

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { Product, ProductService } from './product.service';
+import { Product, ProductStore } from './product.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 import { CardComponent } from './card.component';
@@ -21,7 +21,7 @@ import { ActivatedRoute } from '@angular/router';
       </select>
     </div>
     <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-      @for (product of (products$ | async)?.products; track product.id) {
+      @for (product of products(); track product.id) {
         <app-product-card
           [product]="product"
           (addToCart)="addToCart($event)"
@@ -38,7 +38,7 @@ import { ActivatedRoute } from '@angular/router';
         <span
           aria-current="page"
           class="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-          >{{ productService.page$ | async }}</span
+          >{{ productStore.page() }}</span
         >
 
         <span
@@ -52,38 +52,38 @@ import { ActivatedRoute } from '@angular/router';
   styles: ``,
 })
 export class ProductListComponent implements OnInit {
-  protected productService = inject(ProductService);
+  protected productStore = inject(ProductStore);
   protected cartService = inject(CartService);
   private activatedRoute = inject(ActivatedRoute);
 
-  protected products$ = this.productService.products$;
+  protected products = this.productStore.sortedProducts;
   private readonly destroy: DestroyRef = inject(DestroyRef);
-  sortBy = new FormControl(this.productService.getCurrentSortValue());
+  sortBy = new FormControl(this.productStore.sort());
 
   ngOnInit(): void {
     this.sortBy.valueChanges
       .pipe(filter(Boolean), takeUntilDestroyed(this.destroy))
-      .subscribe((sortBy) => this.productService.setSortValue(sortBy));
+      .subscribe((sortBy) => this.productStore.setSort(sortBy));
 
     this.activatedRoute.queryParams
       .pipe(
-        finalize(() => this.productService.setSearchQuery('')),
+        finalize(() => this.productStore.setSearch('')),
         takeUntilDestroyed(this.destroy),
       )
       .subscribe((params) => {
         const { search } = params;
         if (search) {
-          this.productService.setSearchQuery(search);
+          this.productStore.setSearch(search);
         }
       });
   }
 
   incresePage() {
-    this.productService.increasePage();
+    this.productStore.increasePage();
   }
 
   decresePage() {
-    this.productService.decreasePage();
+    this.productStore.decreasePage();
   }
 
   addToCart(product: Product) {
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,6 +1,15 @@
-import { inject, Injectable } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, catchError, map, of, switchMap } from 'rxjs';
+import { catchError, filter, of, pipe, switchMap, tap } from 'rxjs';
+import {
+  patchState,
+  signalStore,
+  withComputed,
+  withHooks,
+  withMethods,
+  withState,
+} from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
 
 export type Product = {
   id: string;
@@ -28,89 +37,107 @@ export type ProductSort = 'alphabetically' | 'price-asc' | 'price-desc';
 /**
  * https://dummyjson.com/docs/products
  */
-@Injectable({ providedIn: 'root' })
-export class ProductService {
-  private readonly itemsPerPage = 8;
-
-  private httpClient = inject(HttpClient);
-  private search = new BehaviorSubject('');
-
-  private sortSub = new BehaviorSubject<ProductSort>('alphabetically');
-  private paginationSub = new BehaviorSubject(1);
-
-  public page$ = this.paginationSub.asObservable();
-
-  public products$ = this.page$.pipe(
-    switchMap((page) =>
-      this.getProducts(page).pipe(
-        switchMap((products) => {
-          return this.sortSub.asObservable().pipe(
-            map((sortBy) => {
-              const prods = products?.products ?? [];
-              return {
-                ...products,
-                products: [...prods].sort((a, b) => {
-                  if (sortBy === 'alphabetically') {
-                    return a.title.localeCompare(b.title);
-                  } else if (sortBy === 'price-asc') {
-                    return a.price - b.price;
-                  } else {
-                    return b.price - a.price;
-                  }
-                }),
-              };
-            }),
-          );
-        }),
-      ),
-    ),
-  );
-
-  public getCurrentSortValue() {
-    return this.sortSub.value;
-  }
-
-  public setSortValue(sortBy: ProductSort) {
-    this.sortSub.next(sortBy);
-  }
-
-  public getProducts(page: number) {
-    const skip = this.itemsPerPage * (page - 1);
-    return this.search.pipe(
-      switchMap((search) => {
-        return this.httpClient
-          .get<Products>(
-            `https://dummyjson.com/products/search?limit=${this.itemsPerPage}&skip=${skip}&q=${search}`,
-          )
-          .pipe(
-            catchError((x) =>
-              of({
-                products: [],
-                total: 0,
-                skip: skip,
-                limit: this.itemsPerPage,
-              } as Products),
-            ),
-          );
+export const ProductStore = signalStore(
+  { providedIn: 'root' },
+  withState({
+    page: 1,
+    sort: 'alphabetically',
+    search: '',
+    selectedProduct: undefined,
+    selectedProductId: '',
+    products: [],
+  } as {
+    page: number;
+    sort: ProductSort;
+    search: string;
+    selectedProduct: Product | undefined;
+    selectedProductId: string;
+    products: Product[];
+  }),
+  withComputed(({ products, sort, search, page }) => {
+    return {
+      sortedProducts: computed(() => {
+        const sortBy = sort();
+        return [...products()].sort((a, b) => {
+          if (sortBy === 'alphabetically') {
+            return a.title.localeCompare(b.title);
+          } else if (sortBy === 'price-asc') {
+            return a.price - b.price;
+          } else {
+            return b.price - a.price;
+          }
+        });
       }),
-    );
-  }
-
-  increasePage() {
-    this.paginationSub.next(this.paginationSub.value + 1);
-  }
-
-  decreasePage() {
-    this.paginationSub.next(this.paginationSub.value - 1);
-  }
-
-  getProduct(id: string) {
-    return this.httpClient
-      .get<Product>(`https://dummyjson.com/products/${id}`)
-      .pipe(catchError((e) => of(undefined)));
-  }
-
-  setSearchQuery(query: string) {
-    this.search.next(query);
-  }
-}
+      pageAndSort: computed(() => ({
+        search: search(),
+        page: page(),
+      })),
+    };
+  }),
+  withMethods((state) => {
+    const httpClient = inject(HttpClient);
+    const itemsPerPage = 8;
+    return {
+      setSelectedProduct(productId: string): void {
+        patchState(state, { selectedProductId: productId });
+      },
+      setSort(sort: ProductSort): void {
+        patchState(state, { sort });
+      },
+      increasePage() {
+        patchState(state, { page: state.page() + 1 });
+      },
+      decreasePage() {
+        patchState(state, { page: state.page() - 1 });
+      },
+      setSearch(search: string): void {
+        patchState(state, { search });
+      },
+      getProducts: rxMethod<{
+        page: number;
+        search: string;
+      }>(
+        pipe(
+          switchMap(({ page, search }) => {
+            const skip = itemsPerPage * (page - 1);
+            return httpClient
+              .get<Products>(
+                `https://dummyjson.com/products/search?limit=${itemsPerPage}&skip=${skip}&q=${search}`,
+              )
+              .pipe(
+                catchError((x) =>
+                  of({
+                    products: [],
+                    total: 0,
+                    skip: skip,
+                    limit: itemsPerPage,
+                  } as Products),
+                ),
+              );
+          }),
+          tap((_products) => {
+            const products = _products.products;
+            patchState(state, { products });
+          }),
+        ),
+      ),
+      getProduct: rxMethod<string>(
+        pipe(
+          filter((id) => !!id),
+          switchMap((id) =>
+            httpClient
+              .get<Product>(`https://dummyjson.com/products/${id}`)
+              .pipe(catchError((e) => of(undefined))),
+          ),
+          tap((product) => patchState(state, { selectedProduct: product })),
+        ),
+      ),
+    };
+  }),
+  withHooks({
+    onInit({ getProduct, selectedProductId, getProducts, pageAndSort }) {
+      getProduct(selectedProductId);
+      getProducts(pageAndSort);
+    },
+  }),
+);
